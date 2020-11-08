5b14fc23c3087851300b50b31d699aaff4fe37b8
InkTheGreat <41271192+InkTheGreat@users.noreply.github.com>
2020-11-08 14:07:01 -0600

    <files>

---
+++ Project 8/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project_8.Models;
+
+namespace Project_8.Controllers
+{
+   public class HomeController : Controller
+   {
+      private readonly ILogger<HomeController> _logger;
+
+      public HomeController(ILogger<HomeController> logger)
+      {
+         _logger = logger;
+      }
+
+      public IActionResult index()
+      {
+         return View("SignIn");
+      }
+      [HttpGet]
+      public IActionResult SignUp()
+      {
+         return View("SignUp");
+      }
+      [HttpPost]
+      public IActionResult SignUp(User i)
+      {
+         if (ModelState.IsValid)
+         {
+            return View("UserDetails", i);
+         }
+         else
+         {
+            return View(i);
+         }
+        }
+      [HttpGet]
+      public IActionResult EditUser()
+      {
+         User p = new User();
+         p.Fname = "Paul";
+         p.Lname = "Musili";
+         p.Uname = "Musilip";
+         p.Tname = "Mr";
+
+         return View(p); 
+      }
+
+
+      [HttpPost]
+      public IActionResult EditUser(User Info)
+      {
+         if (ModelState.IsValid)
+         {
+            return View("UserDetails", Info);
+         }
+         else
+         {
+            return View(Info);
+         }
+
+      }
+
+
+
+      public IActionResult Privacy()
+      {
+         return View();
+      }
+
+      [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+      public IActionResult Error()
+      {
+         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      }
+   }
+}
---
+++ Project 8/Models/NewUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_8.Models
+{
+   public class User
+   {
+      //   F i e l d s   &   P r o p e r t i e s
+
+      public int Id { get; set; }
+      [Required(ErrorMessage = "First Name Required")]
+      public string Fname { get; set; }
+      [Required(ErrorMessage = "Last Name Required")]
+      public string Lname { get; set; }
+      [Required(ErrorMessage = "UserName Invalid")]
+      public string Uname { get; set; }
+      //Title name will be set to N/a if left blank.
+      public string Tname {get; set; }
+      [Required(ErrorMessage = "Email is required")]
+      public string Email { get; set; }
+      [Required(ErrorMessage = "Please Confirm Email")]
+      public string CEmail { get; set; }
+      [Required(ErrorMessage = "Password is Required")]
+      public string Password { get; set; }
+      [Required(ErrorMessage = "Please Confirm Email")]
+      public string CPassword { get; set; }
+
+
+      //   C o n s t r u c t o r s
+
+      //   M e t h o d s
+   }
+}
